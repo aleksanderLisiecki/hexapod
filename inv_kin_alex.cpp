16a5c936af4063d919194f7743581a3e55b09093
Alex <alilis@interia.pl>
2019-01-28 12:53:21 +0100

    działająca kinematyka alexa: na płaszczyźnie, obsluguje katy ujemne

--- inv_kin_alex.cpp
+++ inv_kin_alex.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
+#include <cmath>
 
 #define PI 3.14159265
+
+#define debug(str) do { std::cout << "debug: " << str << std::endl; } while( false )
+//to print debug message use: "debug('message');"
 
 using namespace std;
 
@@ -11,20 +14,34 @@ using namespace std;
 int a = 90;
 int b = 180;
 
-
 vector<double> inv_kin(double, double);
+double theta1(double x, double y, double theta2);
+double theta2(double x, double y);
+
+
+
 
 int main(){
     vector<double> v;
     double x, y;
+/*
+    for(int iy=0; iy<=270; iy+5){
+        for(int ix=0; ix<=270; ix+5){
+            inv_kin(ix, iy);
+        }
+        cout << endl;
+    }
+*/
 
-    while(1)
-    {
-    cout << "Podaj x, y" << endl;
+    do{
+    cout << "Podaj x, y" << endl <<"x: ";
     cin >> x;
+    cout << "y: ";
     cin >> y;
     inv_kin(x,y);
-    }
+    }while(1);
+
+
     return 0;
 }
 
@@ -32,14 +49,48 @@ double k(double zmienna_kwadrat){
     return zmienna_kwadrat * zmienna_kwadrat;
     }
 
-vector<double> inv_kin(double y, double x){
+vector<double> inv_kin(double x, double y){
+    double t1;
+    double t2 = theta2(x,y);
+    t1 = theta1(x, y, t2);
 
-    double theta2 = (acos(((x*x)+(y*y)-(a*a)-(b*b))/(2*a*b)))* (180.0) / PI;
+    vector<double> vct_inv_kin;
+    vct_inv_kin.push_back(t1);
+    vct_inv_kin.push_back(t2);
 
-    cout << theta2;
+    cout << "theta1: " << t1 << endl << "theta2: " << t2 << endl;
 
-    double alfa = atan(x/y);
+    if(theta1 > 180 || theta2 > 180){
+        cout << "UWAGA!!! - kat wiekszy niz 180" << endl;
+    }
 
-
-
+    //return vct_inv_kin;
 }
+
+double theta1(double x, double y, double theta2){
+
+    double alfa = abs(atan(x/y) * (180.0) / PI);
+
+    debug(alfa);
+
+    double c = sqrt((x*x) + (y*y));
+    double beta;
+    double beta_asin_licznik = (sin(theta2 * PI / 180.0)) * b;
+    beta = asin(beta_asin_licznik / c) * (180.0) / PI;
+
+    debug(beta);
+
+    if(y < 0){
+        return alfa + 2*(90-alfa) + beta;
+    }else{
+        return alfa + beta;
+    }
+}
+
+double theta2(double x, double y){
+    double theta2_acos_licznik = (x*x)+(y*y)-(a*a)-(b*b);
+    double theta2_acos_mianownik = 2 * a * b;
+    return (acos(theta2_acos_licznik / theta2_acos_mianownik)) * (180.0) / PI;
+}
+
+
